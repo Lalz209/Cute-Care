a51d997a55421eac78e4e44bf8cb6273e072c4b0
Lalz209 <lalzpaisa@gmail.com>
2025-01-14 12:36:47 -0600

    page will all working until now, but grid. Working on  it

---
+++ src/App.js
@@ -0,0 +1,94 @@
+import './App.css';
+import React, {useState, useEffect} from 'react';
+import SlidingTextBar from './components/SlidingTextBar';
+
+function App() {
+    const [currentIndex, setCurrentIndex] = useState(0);
+    const itemsPerPage = 6;
+    const itemWidth = 285;
+
+
+    const gridItems = [
+      'recurso 11.png', 'recurso 11.png', 'recurso 11.png', 
+      'recurso 11.png', 'recurso 11.png', 'recurso 11.png', 
+      'recurso 11.png', 'recurso 11.png', 'recurso 11.png',
+      'recurso 11.png','recurso 11.png', 'recurso 11.png',
+      'recurso 11.png'];
+
+    const maxPages = Math.ceil(gridItems.length / itemsPerPage);
+
+    const visibleItems = gridItems.slice(
+      currentIndex * itemsPerPage,
+      (currentIndex + 1) * itemsPerPage
+    );
+  
+    useEffect(() => {
+      const grid = document.querySelector('.grid');
+      const offset = -(currentIndex * itemsPerPage * itemWidth);
+      if (grid) {
+        grid.style.transform = `translateX(${offset}px)`;
+      }
+    }, [currentIndex, itemsPerPage, itemWidth]);
+  
+    const handleNext = () => {
+      if (currentIndex < maxPages - 1) {
+        setCurrentIndex(currentIndex + 1);
+      }
+    };
+  
+    const handlePrev = () => {
+      if (currentIndex > 0) {
+        setCurrentIndex(currentIndex - 1);
+      }
+    };
+
+  return (
+    <div className="App">
+      <div className='nav'>
+        <div className='logo-nav'>
+          <img className='img-logo-nav' src='recurso 5.png' alt='logo'/>
+        </div>
+        <div className='nav-social-media'>
+          <a target="_blank" href='https://wa.me/523310684078?text=Hola!%20Me%20interesa%20informaci%C3%B3n'><img className='social-img' src='recurso 6.png' alt='wpp'/></a>
+          <a target="_blank" className='social-img' href='https://www.instagram.com/cuteandcare.db/'><img className='social-img' src='recurso 7.png' alt='instagram'/></a>
+         
+        </div>
+      </div>
+      <div className='header'>
+        <div className='main-img-container'>
+          <img className='main-img' src='recurso 1.png' alt='p'/>
+        </div>
+        <div className='main-2-container'>
+          <img className='img-p2' src='recurso 3.png' alt='p 2'/>
+          <a className='sch-img-cont' target="_blank" href='https://wa.me/523310684078?text=Hola!%20Me%20interesa%20informaci%C3%B3n'><img  className='schedule-img' src='recurso 8.png' alt='wpp'/></a>
+        </div>
+      </div>
+      <div>
+        <SlidingTextBar />
+      </div>
+      <div className='service-img-container'>
+        <img className='Service-img' src='recurso 10.png' alt='service'/>
+      </div>
+      <div className='grid-container'>
+        <div className='nav-btns'>
+          <button className='nav-btn-left' onClick={handlePrev}> <img className='btn-img' src='felcha iz.png'></img> </button>
+        </div>
+        <div className='grid-wrapper' style={{ width: `${itemsPerPage * itemWidth}px` }}>
+          <div className='grid'>
+            {visibleItems.map((src, index) => (
+              <div className='grid-item' key={index}>
+                <img className='grid-img' src={src} alt='service img' />
+              </div>
+            ))}
+          </div>
+        </div>
+        <div className='nav-btns'>
+          <button className='nav-btn-right' onClick={handleNext}> <img className='btn-img' src='flecha de.png'></img> </button>
+        </div>
+      </div>
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/SlidingTextBar.js
@@ -0,0 +1,41 @@
+import React, { useState, useEffect } from "react";
+import "./SlidingTextBar.css";
+
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css"></link>
+const SlidingTextBar = () => {
+  const texts = ["Contáctame para conocer las promos del mes", "En mi self care era"];
+
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentIndex((prevIndex) => (prevIndex + 1) % texts.length);
+    }, 10000); 
+
+    return () => clearInterval(interval);
+  }, [texts.length]);
+
+  return (
+
+    
+    <div className="sliding-text-bar">
+      <div className="text-container">
+      
+        <span
+        
+          key={currentIndex} 
+          className="sliding-text active"
+        >
+          <img className='img-logo-nav' src='Isotipo.png' alt='logo'/>
+          {texts[currentIndex]}
+          
+        </span>
+      </div>
+    </div>
+
+
+  );
+};
+
+export default SlidingTextBar;
+

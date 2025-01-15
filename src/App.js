04f4e1d41da9dfca96233c15ab00a0fb20e55734
Lalz209 <lalzpaisa@gmail.com>
2025-01-15 13:49:22 -0600

    minimum changes

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import SlidingTextBar from './components/SlidingTextBar';
 function App() {
     const [currentIndex, setCurrentIndex] = useState(0);
     const itemsPerPage = 6;
-    const itemWidth = 285;
+    const itemWidth = 400;
 
 
     const gridItems = [
@@ -17,18 +17,20 @@ function App() {
 
     const maxPages = Math.ceil(gridItems.length / itemsPerPage);
 
-    const visibleItems = gridItems.slice(
+    /*const visibleItems = gridItems.slice(
       currentIndex * itemsPerPage,
       (currentIndex + 1) * itemsPerPage
-    );
+    );*/
   
-    useEffect(() => {
+    /*useEffect(() => {
       const grid = document.querySelector('.grid');
       const offset = -(currentIndex * itemsPerPage * itemWidth);
       if (grid) {
+        grid.style.width = `${gridItems.length * itemWidth}px`;
         grid.style.transform = `translateX(${offset}px)`;
+
       }
-    }, [currentIndex, itemsPerPage, itemWidth]);
+    }, [currentIndex, itemsPerPage, itemWidth, gridItems.length]);*/
   
     const handleNext = () => {
       if (currentIndex < maxPages - 1) {
@@ -69,22 +71,30 @@ function App() {
       <div className='service-img-container'>
         <img className='Service-img' src='recurso 10.png' alt='service'/>
       </div>
-      <div className='grid-container'>
-        <div className='nav-btns'>
-          <button className='nav-btn-left' onClick={handlePrev}> <img className='btn-img' src='felcha iz.png'></img> </button>
-        </div>
-        <div className='grid-wrapper' style={{ width: `${itemsPerPage * itemWidth}px` }}>
-          <div className='grid'>
-            {visibleItems.map((src, index) => (
-              <div className='grid-item' key={index}>
-                <img className='grid-img' src={src} alt='service img' />
+
+          {/*'Grid Container'*/}
+
+          <div className="grid-container">
+        <button className="nav-btn-left" onClick={handlePrev}>
+          <img className="btn-img" src="felcha iz.png" alt="Left arrow" />
+        </button>
+        <div className="grid-wrapper">
+          <div
+            className="grid"
+            style={{
+              transform: `translateX(-${currentIndex * itemsPerPage * itemWidth}px)`,
+            }}
+          >
+            {gridItems.map((src, index) => (
+              <div className="grid-item" key={index}>
+                <img className="grid-img" src={src} alt={`img ${index + 1}`} />
               </div>
             ))}
           </div>
         </div>
-        <div className='nav-btns'>
-          <button className='nav-btn-right' onClick={handleNext}> <img className='btn-img' src='flecha de.png'></img> </button>
-        </div>
+        <button className="nav-btn-right" onClick={handleNext}>
+          <img className="btn-img" src="flecha de.png" alt="Right arrow" />
+        </button>
       </div>
       
     </div>
